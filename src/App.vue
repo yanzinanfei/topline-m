ad93e8fda0a8274f20ae18c0985754bbe1c7d41b
yangshuai <286319700@qq.com>
2020-01-05 17:09:54 +0800

    调整初始目录结构

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <h1>521</h1>
+    <router-view></router-view>
   </div>
 </template>
 
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
 <style lang="less">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
