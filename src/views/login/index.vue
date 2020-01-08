98124686991e8b0704d2f101817229336f074592
yangshuai <286319700@qq.com>
2020-01-08 19:57:02 +0800

    实现基本登录功能

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,12 +5,14 @@
     <!-- 导航栏结束 -->
     <!-- 登录表单开始 -->
     <van-cell-group>
-      <van-field required clearable placeholder="请输入手机号" />
-      <van-field placeholder="请输入验证码" required />
+      <van-field class="iconfont icon-shouji" clearable placeholder="请输入手机号" />
+      <van-field class="iconfont icon-iconfontmima1" placeholder="请输入验证码">
+        <van-button slot="button" size="small" type="primary" round>发送验证码</van-button>
+      </van-field>
     </van-cell-group>
     <!-- 登录表单结束 -->
     <!-- 登录按钮 -->
-    <div>
+    <div class="login-btn-wrap">
       <van-button type="info">登录</van-button>
     </div>
     <!-- 登录按钮 -->
@@ -21,5 +23,14 @@
 export default {}
 </script>
 
-<style>
+<style lang='less' scoped>
+.login-container {
+  .login-btn-wrap {
+    padding: 27px 16px;
+    .van-button {
+      width: 100%;
+      background-color: #6db4fb;
+    }
+  }
+}
 </style>
