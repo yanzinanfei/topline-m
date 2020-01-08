1d099efb07c729f4aa0b26126fb4471223c94d19
yangshuai <286319700@qq.com>
2020-01-08 20:59:59 +0800

    实现基本登录功能

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,22 +5,50 @@
     <!-- 导航栏结束 -->
     <!-- 登录表单开始 -->
     <van-cell-group>
-      <van-field class="iconfont icon-shouji" clearable placeholder="请输入手机号" />
-      <van-field class="iconfont icon-iconfontmima1" placeholder="请输入验证码">
+      <van-field v-model="user.mobile" class="iconfont icon-shouji" clearable placeholder="请输入手机号" />
+      <van-field v-model="user.code" class="iconfont icon-iconfontmima1" placeholder="请输入验证码">
         <van-button slot="button" size="small" type="primary" round>发送验证码</van-button>
       </van-field>
     </van-cell-group>
     <!-- 登录表单结束 -->
     <!-- 登录按钮 -->
     <div class="login-btn-wrap">
-      <van-button type="info">登录</van-button>
+      <van-button type="info" @click="onLogin">登录</van-button>
     </div>
     <!-- 登录按钮 -->
   </div>
 </template>
 
 <script>
-export default {}
+import request from '@/utils/request'
+export default {
+  name: 'LoginPage',
+  data () {
+    return {
+      user: {
+        mobile: '', // 手机号
+        code: '' // 验证码
+      }
+    }
+  },
+  methods: {
+    async onLogin () {
+      // 1.获取表单数据
+      const user = this.user
+      // 2.表单验证
+      // 3.请求登录
+      const res = await request({
+        method: 'POST',
+        url: '/app/v1_0/authorizations',
+        // headers: {}, // 请求头参数
+        // params: {}, // Query 查询参数
+        data: user // body 请求体参数
+      })
+      console.log(res)
+      // 4.根据后端返回结果执行后续业务处理
+    }
+  }
+}
 </script>
 
 <style lang='less' scoped>
