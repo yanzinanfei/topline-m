e61abfdb36865dcd549aecd2e7ad1d9fb13de8fa
yangshuai <286319700@qq.com>
2020-01-11 19:40:56 +0800

    个人中心-TabBar路由组件

--- src/router/index.js
+++ src/router/index.js
@@ -7,7 +7,36 @@ Vue.use(VueRouter)
 const routes = [{
   path: '/login',
   component: Login
-}]
+},
+{
+  path: '/',
+  // 有默认子路由的不需要配置 name
+  // name: 'tab-bar',
+  component: () => import('@/views/tab-bar'),
+  children: [
+    {
+      path: '', // 默认子路由
+      name: 'home',
+      component: () => import('@/views/home')
+    },
+    {
+      path: 'qa',
+      name: 'qa',
+      component: () => import('@/views/qa')
+    },
+    {
+      path: 'video',
+      name: 'video',
+      component: () => import('@/views/video')
+    },
+    {
+      path: 'my',
+      name: 'my',
+      component: () => import('@/views/my')
+    }
+  ]
+}
+]
 
 const router = new VueRouter({
   routes
--- src/utils/register.vant.js
+++ src/utils/register.vant.js
@@ -9,7 +9,9 @@ import {
   NavBar,
   Field,
   Toast,
-  CountDown
+  CountDown,
+  Tabbar,
+  TabbarItem
 } from 'vant'
 
 Vue.use(Button)
@@ -20,3 +22,5 @@ Vue.use(Button)
   .use(Field)
   .use(Toast)
   .use(CountDown)
+  .use(Tabbar)
+  .use(TabbarItem)
