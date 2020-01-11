613098b28bdba1ab2716070e9780ed9ed0d9daaf
yangshuai <286319700@qq.com>
2020-01-11 19:10:36 +0800

    Token处理-发送Token

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,9 @@
 // 封装 axios 请求模块
 import axios from 'axios'
 import jsonBig from 'json-bigint'
-
+// 在非组件模块中直接加载获取容器
+// 这里拿到的 store 和你在组件中访问 this.$store 是一个东西
+import store from '@/store'
 // axios.create 方法;复制一个 axios
 
 const request = axios.create({
@@ -24,5 +26,29 @@ request.defaults.transformResponse = [function (data) {
     return {}
   }
 }]
+// 请求拦截器
+
+request.interceptors.request.use(function (config) {
+  // config 请求配置对象，我们可以通过修改config 来实现统一请求数据处理
+  const { user } = store.state
+  // 统一添加token
+  if (user) {
+    // config.headers 获取操作请求头对象
+    // Authorization 是后端要求的字段名称
+    // 数据值后端要求提供： Bearer token数据
+    // 注意：Bearer 后面有个空格
+    config.headers.Authorization = `Bearer ${user.token}`
+  }
+  return config
+}, function (error) {
+  return Promise.reject(error)
+})
+// 请求拦截器
+
+request.interceptors.response.use(function (response) {
+  return response
+}, function (error) {
+  return Promise.reject(error)
+})
 
 export default request
