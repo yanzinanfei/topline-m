7e1a49fa31472e8d2d75fb451b6d9897f956da1a
yangshuai <286319700@qq.com>
2020-01-15 20:47:25 +0800

    文章搜索-创建组件并配置路由

---
+++ src/views/search/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="search-container">搜索</div>
+</template>
+
+<script>
+export default {
+  name: 'SearchPage',
+  components: {},
+  props: {},
+  data () {
+    return {
+    }
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+<style lang="less" scoped>
+</style>
